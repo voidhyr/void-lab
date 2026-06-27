af41931545fe6da0b6df30095e3a37266c16f752
voidhyr <samdani295@gmail.com>
2026-06-27 20:27:31 +0530

    feat: add first dockerfile and python script for docker fundamentals

---
+++ devops/docker/fundamentals/script.py
@@ -0,0 +1,7 @@
+import platform
+
+
+print("===System information===")
+print(f"OS Name: {platform.system()}")
+print(f"OS Release: {platform.release()}")
+print(f"Architecture: {platform.machine()}")
